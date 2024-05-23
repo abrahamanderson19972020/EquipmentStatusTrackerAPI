a34101752ea3496f52466fb13a34414d82aaa579
abrahamanderson19972020 <abraham.anderson83@gmail.com>
2024-05-23 20:01:07 +0200

    Layered software structure initially is created

---
+++ EquipmentStatusTracker/Entities/Concrete/GpsPosition.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class GpsPosition:IEntity
+    {
+        public int Id { get; set; }
+        public string DeviceId { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
