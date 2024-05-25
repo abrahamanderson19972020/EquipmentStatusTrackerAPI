cd8dbea617f3fe4a13f72490e98e3276b4e88a00
abrahamanderson19972020 <abraham.anderson83@gmail.com>
2024-05-25 12:44:17 +0200

    Data acces layer is configured and added in Entity Framework

---
+++ EquipmentStatusTracker/DataAccess/Abstract/IEntityRepositoryDal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    public interface IEntityRepositoryDal<T> where T : class 
+    {
+        Task<List<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+        Task SaveChangesAsync();
+    }
+}
